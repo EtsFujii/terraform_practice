7214a30b95133463f4c8f05bfcd46099c1015b4c
EtsFujii <etsuko.fujii0407@gmail.com>
2026-08-10 21:56:33 +0900

    update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,11 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-#  # リモートバックエンド（GCS）の設定を追加
-#  backend "gcs" {
-#    bucket = "gcp-practice-504123-tfstate"
-#    prefix = "terraform/state"
-#  }
 }
 
 provider "google" {
